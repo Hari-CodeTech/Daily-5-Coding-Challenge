7fcb20ae36b9ad3747e6aebea16ea110d61e9d93
Hari-CodeTech <puvvalahari99@gmail.com>
2026-07-20 22:12:57 +0530

    Add files via upload

---
+++ Typeconversion.java
@@ -0,0 +1,19 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String num = sc.nextLine();
+        // TypeCast to int double it and print
+        int number=Integer.parseInt(num);
+        int result=number*2;
+        System.out.println(+result);
+        sc.close();
+    }
+}
+
+//{ Driver Code Starts.
+// } Driver Code EndsjavaT y67857u9o9o9ojkkm

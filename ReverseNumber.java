cd256933b6bf74231d1e54d01ac34dad26ab64cf
Hari-CodeTech <puvvalahari99@gmail.com>
2026-07-21 22:16:20 +0530

    Add files via upload

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,25 +1,18 @@
-package com.Loops;
-import java.util.*;
+package com.RecursiveFunction;
+
 public class ReverseNumber {
+	static int reverse(int n,int rev) {
+		if(n==0) {
+			return rev;
+		}
+		int digit=n%10 ;
+		return reverse(n/10,rev*10+digit);
+		 
+	}
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int temp=n;
-		int rev=0;
-		
-		while(n!=0) {
-			int rem=n%10;
-			rev=(rev*10)+rem;
-			n=n/10;
-			}
-		System.out.println(rev);
-		if(temp==rev) {
-			System.out.println("Palindrome number");
-		}
-		else {
-			System.out.println("Not a Palindrome Number");
-		}
+		int res=reverse(154,0);
+		System.out.println(res);
 	}
 
 }
